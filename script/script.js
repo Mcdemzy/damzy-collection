2dc21c12f40d507270d6eb88c93f22dcdc399016
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-03-26 07:29:51 +0100

    Designing Navbar

--- script/script.js
+++ script/script.js
@@ -1,7 +1,21 @@
 // Simulate page loading
 window.addEventListener("load", function () {
-  // Hide the loader and show the content after a delay (for demonstration purposes)
+  // Hide the loader after a delay and show the content
   setTimeout(function () {
-    document.body.classList.add("loaded");
-  }, 6000); // Change the delay time as needed
+    document.getElementById("loader-container").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  }, 2000); // Adjust the delay time as needed
+});
+
+// NAVBAR BEHAVIOUR
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  var navbarOffsetTop = navbar.offsetTop;
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition > navbarOffsetTop) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
 });
