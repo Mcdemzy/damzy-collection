dacfd841109db7fbefbaece7554d0db7ead06aba
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-03-26 13:21:44 +0100

    Designing Hero section

--- script/script.js
+++ script/script.js
@@ -18,3 +18,35 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("fixed");
   }
 });
+
+// ACTIVE BEHAVIOUR
+document.addEventListener("DOMContentLoaded", function () {
+  const navItems = document.querySelectorAll(".navbar ul li a");
+
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("mouseenter", function (event) {
+      const hoveredNavItem = event.target;
+
+      // Remove the 'active__nav-item' class from all nav items
+      navItems.forEach((item) => item.classList.remove("active__nav-item"));
+
+      // Add the 'active__nav-item' class to the hovered nav item
+      hoveredNavItem.classList.add("active__nav-item");
+
+      // Move the underline to the hovered nav item
+      moveUnderline(hoveredNavItem);
+    });
+  });
+
+  function moveUnderline(navItem) {
+    const underline = document.querySelector(
+      ".navbar ul li a.active__nav-item:after"
+    );
+    const navItemPosition = navItem.getBoundingClientRect();
+    const navItemWidth = navItem.offsetWidth;
+
+    // Set the width and position of the underline to match the hovered nav item
+    underline.style.width = `${navItemWidth}px`;
+    underline.style.left = `${navItemPosition.left}px`;
+  }
+});
